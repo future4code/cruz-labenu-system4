256167b69b9e81678ef7c07e888e2fd3fe1f6347
nathaliacardoso <nathalia.mscardoso@gmail.com>
2021-06-04 18:16:01 -0300

    Finalizar validação de turma

--- src/endpoints/create_class.ts
+++ src/endpoints/create_class.ts
@@ -1,14 +1,30 @@
 import {Request, Response} from 'express';
-import {Class} from '../types';
+import {Class, ClassName} from '../types';
 import {connection} from '../connection'
 
 export default async function createClass(req:Request,res:Response){
     const reqBody = req.body
+
     try{
 
-        if(!reqBody.Id_Class || !reqBody.Class_Name || !reqBody.Initial_Date || !reqBody.Final_Date || !reqBody.Module){
+        const Id_Class : number = reqBody.Id_Class;
+        const Class_Name : ClassName = reqBody.Class_Name;
+        const Initial_Date : string = reqBody.Initial_Date;
+        const Final_Date : string = reqBody.Final_Date;
+        const Module : number = reqBody.Module;
+
+
+        if(!Id_Class || !Class_Name || !Initial_Date || !Final_Date || !Module){
             throw new Error ('Please, check your information! You need complete all! of then!')
         }
+        
+        if (!Object.values(ClassName).includes(Class_Name)) {
+            throw new Error ('Please, check your information! You need complete with one of then: EPPS, CRUZ, DUMMONT, MOLINA, JACKSON')
+        }
+    
+        if(reqBody.night === true){
+            reqBody.Class_Name = reqBody.Class_Name + ' na-night'          
+        } 
         const newClass: Class = {
             Id_Class : reqBody.Id_Class,
             Class_Name : reqBody.Class_Name,
